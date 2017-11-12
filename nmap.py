7bd02763b24757e01f14085d5261b906eabdf451
Yoash <yoashos@gmail.com>
2017-11-12 23:28:19 +0200

    python script to run nmap OS detection with options

--- nmap.py
+++ nmap.py
@@ -1,1 +1,11 @@
-# git test
+import os, json
+
+
+def get_host_recon(options, ip):
+    command = "nmap " + options + " " + ip + " -oX - scanme.nmap.org"
+    process = os.popen(command)
+    results = process.read()
+    return results
+
+
+print(get_host_recon('-o', 'ip'))
